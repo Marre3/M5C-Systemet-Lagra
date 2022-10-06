1556662f2818c1520e031556540ac87dbd95245e
Marre3 <marre244@gmail.com>
2022-10-06 03:47:27 +0200

    Bugfix view

--- view-v2.py
+++ view-v2.py
@@ -3,8 +3,8 @@
 
 def view(description, strings):
     print(description)
-    for i in range(len(names)):
-        print(f"    {i + 1}) {names[i]}")
+    for i in range(len(strings)):
+        print(f"    {i + 1}) {strings[i]}")
 
 
 names = ["nisse", "stina", "bosse", "mimmi"]
